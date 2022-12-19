ed0f53d24186c5c1331a9090a84bd87b376c6ead
freetaill <113572099+freetaill@users.noreply.github.com>
2022-12-20 00:59:40 +0900

    대회 수정

--- Assets/Ranking.cs
+++ Assets/Ranking.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class Ranking
 {
     public int[] charm_type = new int[6];
-    public int[] charm_charmming = new int[6];
+    public double[] charm_charmming = new double[6];
     public int[] swim_type = new int[6];
-    public int[] swim_hp = new int[6];
+    public double[] swim_hp = new double[6];
     public int[] hurdle_type = new int[6];
-    public int[] hurdle_speedjump = new int[6];
+    public double[] hurdle_speedjump = new double[6];
 }
